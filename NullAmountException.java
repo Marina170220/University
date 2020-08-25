3696866cfd4748759a3be1f9fafb7aacfd781422
Marina <petrukmn@yandex.ru>
2020-08-25 19:48:13 +0300

    Null amount exception

---
+++ NullAmountException.java
@@ -0,0 +1,12 @@
+public class NullAmountException extends Exception {
+
+    public NullAmountException(String message) {
+        super(message);
+    }
+
+    public NullAmountException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
+
