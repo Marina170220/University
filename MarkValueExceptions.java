8c95efa11ec1ce8ef40c48a26ea9d208f47706eb
Marina <petrukmn@yandex.ru>
2020-08-25 19:47:16 +0300

    Student has an invalid value of mark exception

---
+++ MarkValueExceptions.java
@@ -0,0 +1,10 @@
+public class MarkValueExceptions extends Exception{
+
+    public MarkValueExceptions(String message){
+        super(message);
+    }
+
+    public MarkValueExceptions(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
