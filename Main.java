ce47416b14212fe3e935c48a5e865806d57ea1a3
Marina <petrukmn@yandex.ru>
2020-09-07 12:03:51 +0300

    Call fillGroup method while creating new Group. Add cause in parameters of Exceptions

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MarkValueExceptions, NullAmountException {
 
         List<Student> group11Students = new ArrayList<>();
         List<Student> group12Students = new ArrayList<>();
@@ -20,75 +20,61 @@ public class Main {
         List<Student> group34Students = new ArrayList<>();
         List<Student> group35Students = new ArrayList<>();
 
-        Group group11 = new Group("Group11");
-        Group group12 = new Group("Group12");
-        Group group13 = new Group("Group13");
-
-        Group group21 = new Group("Group21");
-        Group group22 = new Group("Group22");
-        Group group23 = new Group("Group23");
-        Group group24 = new Group("Group24");
-
-        Group group31 = new Group("Group31");
-        Group group32 = new Group("Group32");
-        Group group33 = new Group("Group33");
-        Group group34 = new Group("Group34");
-        Group group35 = new Group("Group35");
-
-        List<List<Student>> faculty1Groups = new ArrayList<>();
-        List<List<Student>> faculty2Groups = new ArrayList<>();
-        List<List<Student>> faculty3Groups = new ArrayList<>();
-
-        List<Faculty> university = new ArrayList<>();
-
         try {
-            faculty1Groups.add(group11.fillGroup(new File("F:\\University\\F1\\Gr11"), group11Students));
-            faculty1Groups.add(group12.fillGroup(new File("F:\\University\\F1\\Gr12"), group12Students));
-            faculty1Groups.add(group13.fillGroup(new File("F:\\University\\F1\\Gr13"), group13Students));
+            Group group11 = new Group(Group.fillGroup((new File("F:\\University\\F1\\Gr11")), group11Students), "Group11");
+            Group group12 = new Group(Group.fillGroup((new File("F:\\University\\F1\\Gr12")), group12Students), "Group12");
+            Group group13 = new Group(Group.fillGroup((new File("F:\\University\\F1\\Gr13")), group13Students), "Group13");
 
-            faculty2Groups.add(group21.fillGroup(new File("F:\\University\\F2\\Gr21"), group21Students));
-            faculty2Groups.add(group22.fillGroup(new File("F:\\University\\F2\\Gr22"), group22Students));
-            faculty2Groups.add(group23.fillGroup(new File("F:\\University\\F2\\Gr23"), group23Students));
-            faculty2Groups.add(group24.fillGroup(new File("F:\\University\\F2\\Gr24"), group24Students));
+            Group group21 = new Group(Group.fillGroup((new File("F:\\University\\F2\\Gr21")), group21Students), "Group21");
+            Group group22 = new Group(Group.fillGroup((new File("F:\\University\\F2\\Gr22")), group22Students), "Group22");
+            Group group23 = new Group(Group.fillGroup((new File("F:\\University\\F2\\Gr23")), group23Students), "Group23");
+            Group group24 = new Group(Group.fillGroup((new File("F:\\University\\F2\\Gr24")), group24Students), "Group24");
 
-            faculty3Groups.add(group31.fillGroup(new File("F:\\University\\F3\\Gr31"), group31Students));
-            faculty3Groups.add(group32.fillGroup(new File("F:\\University\\F3\\Gr32"), group32Students));
-            faculty3Groups.add(group33.fillGroup(new File("F:\\University\\F3\\Gr33"), group33Students));
-            faculty3Groups.add(group34.fillGroup(new File("F:\\University\\F3\\Gr34"), group34Students));
-            faculty3Groups.add(group35.fillGroup(new File("F:\\University\\F3\\Gr35"), group35Students));
-
-        } catch (NullAmountException e) {
-            System.err.println(e.getMessage());
-        }
-
-        Faculty faculty1 = new Faculty(faculty1Groups, "Faculty1");
-        Faculty faculty2 = new Faculty(faculty2Groups, "Faculty2");
-        Faculty faculty3 = new Faculty(faculty3Groups, "Faculty3");
-
-        university.add(faculty1);
-        university.add(faculty2);
-        university.add(faculty3);
+            Group group31 = new Group(Group.fillGroup((new File("F:\\University\\F3\\Gr31")), group31Students), "Group31");
+            Group group32 = new Group(Group.fillGroup((new File("F:\\University\\F3\\Gr32")), group32Students), "Group32");
+            Group group33 = new Group(Group.fillGroup((new File("F:\\University\\F3\\Gr33")), group33Students), "Group33");
+            Group group34 = new Group(Group.fillGroup((new File("F:\\University\\F3\\Gr34")), group34Students), "Group34");
+            Group group35 = new Group(Group.fillGroup((new File("F:\\University\\F3\\Gr35")), group35Students), "Group35");
 
 
-        try {
-            group11.studentAverageMark(group11Students, "1103");
-        } catch (NullAmountException e) {
-            System.err.println(e.getMessage());
-        }
-        try {
-            group11.averageMarkOnSubjectInGroup(group11Students, "Math");
-        } catch (NullAmountException e) {
-            System.err.println(e.getMessage());
-        }
-        try {
+            List<Group> faculty1Groups = new ArrayList<>();
+            List<Group> faculty2Groups = new ArrayList<>();
+            List<Group> faculty3Groups = new ArrayList<>();
+
+            List<Faculty> university = new ArrayList<>();
+
+            faculty1Groups.add(group11);
+            faculty1Groups.add(group12);
+            faculty1Groups.add(group13);
+
+            faculty2Groups.add(group21);
+            faculty2Groups.add(group22);
+            faculty2Groups.add(group23);
+            faculty2Groups.add(group24);
+
+            faculty3Groups.add(group31);
+            faculty3Groups.add(group32);
+            faculty3Groups.add(group33);
+            faculty3Groups.add(group34);
+            faculty3Groups.add(group35);
+
+            Faculty faculty1 = new Faculty(faculty1Groups, "Faculty1");
+            Faculty faculty2 = new Faculty(faculty2Groups, "Faculty2");
+            Faculty faculty3 = new Faculty(faculty3Groups, "Faculty3");
+
+            university.add(faculty1);
+            university.add(faculty2);
+            university.add(faculty3);
+
+            group11.studentAverageMark(group11Students, "1101");
+            group12.averageMarkOnSubjectInGroup(group12Students, "Math");
             faculty1.averageMarkOnSubjectOnFaculty(faculty1Groups, "History");
-        } catch (NullAmountException e) {
-            System.err.println(e.getMessage());
-        }
-        try {
             University.averageMarkOnSubjectInUniversity(university, "History");
+        } catch (MarkValueExceptions ex) {
+            throw new MarkValueExceptions(ex.getMessage(), ex);
+
         } catch (NullAmountException e) {
-            System.err.println(e.getMessage());
+            throw new NullAmountException(e.getMessage(), e);
         }
     }
 }
