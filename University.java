e6cd60f6deedf17b72e21925698cf17d2d24111b
Marina <petrukmn@yandex.ru>
2020-09-07 11:59:11 +0300

    Add MarkValueException to a signature of all methods. Add cheking of an incorrect student's mark value, an empty student's MarkAndSubjects list, an empty group in all methods. Change List<List<Students>> to List<Group> in Faculty

--- University.java
+++ University.java
@@ -25,29 +25,41 @@ import java.util.*;
 
 public class University {
 
-    public static void averageMarkOnSubjectInUniversity(List<Faculty> university, String subject) throws NullAmountException {
+    public static void averageMarkOnSubjectInUniversity(List<Faculty> university, String subject) throws NullAmountException, MarkValueExceptions {
         double averageMarkOnSubject;
         double sum = 0.0;
         int amountOfMarks = 0;
-        if (university.size() == 0) {
+        if (university.isEmpty()) {
             throw new NullAmountException("The University has no faculties");
         } else {
             for (Faculty faculty : university) {
+                if (faculty.faculty.isEmpty()) {
+                    throw new NullAmountException("Faculty " + faculty.facultyName + " has no groups");
+                }
+
                 for (int i = 0; i < faculty.getFaculty().size(); i++) {
-                    for (int j = 0; j < faculty.getFaculty().get(i).size(); j++) {
-                        if (faculty.getFaculty().get(i).get(j).getMarksAndSubjects().containsKey(subject)) {
-                            sum += faculty.getFaculty().get(i).get(j).getMarksAndSubjects().get(subject);
+                    for (int j = 0; j < faculty.getFaculty().get(i).group.size(); j++) {
+                        if (faculty.getFaculty().get(i).group.get(j).getMarksAndSubjects().isEmpty()) {
+                            throw new NullAmountException("Student " + faculty.getFaculty().get(i).group.get(j).getId() + " has no subjects");
+                        }
+                        if (faculty.getFaculty().get(i).group.get(j).getMarksAndSubjects().values().iterator().next() < 0.0 || faculty.getFaculty().get(i).group.get(j).getMarksAndSubjects().values().iterator().next() > 10.0) {
+                            throw new MarkValueExceptions("Student average mark error.\nCheck student's " + faculty.getFaculty().get(i).group.get(j).getId() + " marks");
+                        }
+
+                        if (faculty.getFaculty().get(i).group.get(j).getMarksAndSubjects().containsKey(subject)) {
+                            sum += faculty.getFaculty().get(i).group.get(j).getMarksAndSubjects().get(subject);
                             amountOfMarks++;
                         }
                     }
                 }
             }
-        }
-        if (amountOfMarks > 0) {
-            averageMarkOnSubject = sum / amountOfMarks;
-            System.out.println("\nAverage mark in University on " + subject + ": " + averageMarkOnSubject);
-        } else {
-            System.out.println("\nThere is no subject " + subject + " in the University");
+
+            if (amountOfMarks > 0) {
+                averageMarkOnSubject = sum / amountOfMarks;
+                System.out.println("\nAverage mark in University on " + subject + ": " + averageMarkOnSubject);
+            } else {
+                System.out.println("\nThere is no subject " + subject + " in the University");
+            }
         }
     }
 
@@ -74,157 +86,183 @@ public class University {
             System.out.println("Student not found");
         }
     }*/
+}
+
+
+class Faculty extends University {
+    String facultyName;
+    List<Group> faculty;
+
+    Faculty(List<Group> faculty, String facultyName) {
+        this.faculty = faculty;
+        this.facultyName = facultyName;
     }
 
-
-    class Faculty extends University {
-        String facultyName;
-        List<List<Student>> faculty;
-
-        Faculty(List<List<Student>> faculty, String facultyName) {
-            this.faculty = faculty;
-            this.facultyName = facultyName;
-        }
-
-        public List<List<Student>> getFaculty() {
-            return faculty;
-        }
-
-        public double averageMarkOnSubjectOnFaculty(List<List<Student>> faculty, String subject) throws NullAmountException {
-            double averageMarkOnSubject = 0.0;
-            double sum = 0.0;
-            int amountOfMarks = 0;
-            if (faculty.isEmpty()) {
-                throw new NullAmountException("Faculty " + facultyName + " has no groups");
-            } else {
-                for (List<Student> group : faculty) {
-                    if (group.isEmpty()) {
-                        throw new NullAmountException("Faculty " + facultyName + " contains empty group");
-                    } else {
-                        for (int i = 0; i < group.size(); i++) {
-                            if (group.get(i).getMarksAndSubjects().isEmpty()) {
-                                throw new NullAmountException("Student " + group.get(i).getId() + " has no subjects");
-                            } else {
-                                if (group.get(i).getMarksAndSubjects().containsKey(subject)) {
-                                    sum += group.get(i).getMarksAndSubjects().get(subject);
-                                    amountOfMarks++;
-                                }
-                            }
-                        }
-                    }
-                }
-                if (amountOfMarks == 0) {
-                    System.out.println("\nThere is no subject " + subject + " on faculty " + facultyName);
-                } else {
-                    averageMarkOnSubject = sum / amountOfMarks;
-                    System.out.println("\nAverage mark on " + subject + " on faculty " + facultyName + ": " + averageMarkOnSubject);
-                }
-            }
-            return averageMarkOnSubject;
-        }
-
-        @Override
-        public String toString() {
-            return "Faculty{" +
-                    "faculty=" + faculty +
-                    '}';
-        }
+    public List<Group> getFaculty() {
+        return faculty;
     }
 
-    class Group extends University {
-
-        String groupName;
-        List<Student> group;
-
-        Group(String groupName) {
-            this.groupName = groupName;
-        }
-
-        @Override
-        public String toString() {
-            return "Group{" +
-                    "group=" + group +
-                    '}';
-        }
-
-        public List<Student> fillGroup(File groupDirectory, List<Student> group) throws NullAmountException {
-            Scanner scanner = null;
-
-            if (groupDirectory.isDirectory()) {
-                if (groupDirectory.listFiles().length == 0) {
-                    throw new NullAmountException("Group " + groupDirectory.getName() + " is empty");
-                } else {
-                    for (File studentFile : groupDirectory.listFiles()) {
-                        try {
-                            scanner = new Scanner(studentFile);
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
-                        }
-                        Map<String, Double> marksAndSubjects = new HashMap<>();
-                        while (scanner.hasNext()) {
-                            marksAndSubjects.put(scanner.next(), scanner.nextDouble());
-                            group.add(new Student(studentFile.getName().substring(0, 4), marksAndSubjects));
-                        }
-                    }
+    public double averageMarkOnSubjectOnFaculty(List<Group> faculty, String subject) throws NullAmountException, MarkValueExceptions {
+        double averageMarkOnSubject = 0.0;
+        double sum = 0.0;
+        int amountOfMarks = 0;
+        if (faculty.isEmpty()) {
+            throw new NullAmountException("Faculty " + facultyName + " has no groups");
+        } else {
+            for (Group group : faculty) {
+                if (group.group.isEmpty()) {
+                    throw new NullAmountException("Faculty " + facultyName + " contains empty group " + group.groupName);
                 }
-            }
-     
-            //System.out.println("Group " + groupDirectory.getName() + ": " + group);
-            return group;
-        }
-
-        public void studentAverageMark(List<Student> group, String id) throws NullAmountException {
-            double averageMark = 0.0;
-            int amountOfMarks = 0;
-            Student findStudent = null;
-            try {
-                for (Student student : group) {
-                    if (student.getMarksAndSubjects().size() == 0) {
-                        throw new NullAmountException("Student " + student.getId() + " has no subjects");
+                for (int i = 0; i < group.group.size(); i++) {
+                    if (group.group.get(i).getMarksAndSubjects().isEmpty()) {
+                        throw new NullAmountException("Student " + group.group.get(i).getId() + " has no subjects");
                     } else {
-                        if (student.getId().contains(id)) {
-                            findStudent = student;
-                            averageMark = findStudent.averageMark(findStudent.getMarksAndSubjects());
+                        if (group.group.get(i).getMarksAndSubjects().values().iterator().next() < 0.0 || group.group.get(i).getMarksAndSubjects().values().iterator().next() > 10.0) {
+                            throw new MarkValueExceptions("Student average mark error.\nCheck student's " + group.group.get(i).getId() + " marks");
                         }
-                        amountOfMarks++;
-                    }
-                }
 
-                if (amountOfMarks > 0) {
-                    System.out.println("\nStudent " + findStudent.getId() + " Average mark: " + averageMark);
-                } else {
-                    System.out.println("\nThere is no student " + id + "  in group " + groupName);
-                }
-            } catch (MarkValueExceptions ex) {
-                System.err.println(ex.getMessage());
-                System.err.println("Student average mark error. Check student's " + findStudent.getId() + " marks");
-            }
-        }
-
-        public void averageMarkOnSubjectInGroup(List<Student> group, String subject) throws NullAmountException {
-            double sum = 0.0;
-            int amountOfMarks = 0;
-
-            for (Student student : group) {
-                if (student.getMarksAndSubjects().size() == 0) {
-                    throw new NullAmountException("Student " + student.getId() + " has no subjects");
-                } else {
-                    if (student.getMarksAndSubjects().containsKey(subject)) {
-                        sum += student.getMarksAndSubjects().get(subject);
-                        amountOfMarks++;
+                        if (group.group.get(i).getMarksAndSubjects().containsKey(subject)) {
+                            sum += group.group.get(i).getMarksAndSubjects().get(subject);
+                            amountOfMarks++;
+                        }
                     }
                 }
             }
+
             if (amountOfMarks == 0) {
-                System.out.println("\nThere is no subject " + subject + " in group " + groupName);
+                System.out.println("\nThere is no subject " + subject + " on faculty " + facultyName);
             } else {
-                double averageMarkOnSubject = sum / amountOfMarks;
-                System.out.println("\nAverage mark on " + subject + " in group " + groupName + ": " + averageMarkOnSubject);
+                averageMarkOnSubject = sum / amountOfMarks;
+                System.out.println("\nAverage mark on " + subject + " on faculty " + facultyName + ": " + averageMarkOnSubject);
             }
+        }
+        return averageMarkOnSubject;
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "faculty=" + faculty +
+                '}';
+    }
+}
+
+class Group extends University {
+
+    String groupName;
+    List<Student> group;
+
+    Group(List<Student> group, String groupName) {
+        this.group = group;
+        this.groupName = groupName;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "group=" + group +
+                '}';
+    }
+
+    public static List<Student> fillGroup(File groupDirectory, List<Student> group) throws NullAmountException, MarkValueExceptions {
+        Scanner scanner = null;
+
+        if (groupDirectory.isDirectory()) {
+            for (File studentFile : groupDirectory.listFiles()) {
+                try {
+                    scanner = new Scanner(studentFile);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                Map<String, Double> marksAndSubjects = new HashMap<>();
+                while (scanner.hasNext()) {
+
+                    marksAndSubjects.put(scanner.next(), scanner.nextDouble());
+                }
+                if (marksAndSubjects.isEmpty()) {
+                    throw new NullAmountException("Student " + studentFile.getName().substring(0, 4) + " has no subjects");
+                }
+                for (Double mark : marksAndSubjects.values()) {
+                    if (mark < 0.0 || mark > 10.0) {
+                        throw new MarkValueExceptions("Invalid mark value in directory " + groupDirectory.getName());
+                    }
+                }
+                group.add(new Student(studentFile.getName().substring(0, 4), marksAndSubjects));
+            }
+        }
+
+
+        if (group.isEmpty()) {
+            throw new NullAmountException("Group " + groupDirectory.getName() + " is empty");
+        }
+        //System.out.println("Group " + groupDirectory.getName() + ": " + group);
+        return group;
+    }
+
+    public void studentAverageMark(List<Student> group, String id) throws NullAmountException, MarkValueExceptions {
+        double averageMark = 0.0;
+        int amountOfMarks = 0;
+        Student findStudent = null;
+        if (group.isEmpty()) {
+            throw new NullAmountException("Group " + groupName + " is empty");
+        }
+        try {
+            for (Student student : group) {
+                if (student.getMarksAndSubjects().isEmpty()) {
+                    throw new NullAmountException("Student " + student.getId() + " has no subjects");
+                }
+                if (student.getId().contains(id)) {
+                    findStudent = student;
+                    averageMark = findStudent.averageMark(findStudent.getMarksAndSubjects());
+                }
+                amountOfMarks++;
+            }
+        } catch (MarkValueExceptions ex) {
+            System.err.println(ex.getMessage());
+            System.err.println("Student average mark error. Check student's " + findStudent.getId() + " marks");
+        }
+
+        if (amountOfMarks > 0) {
+            System.out.println("\nStudent " + findStudent.getId() + " Average mark: " + averageMark);
+        } else {
+            System.out.println("\nThere is no student " + id + "  in group " + groupName);
         }
     }
 
-    /*public void findStudentInGroup(List<Student> group) { //метод не работает!!!
+
+    public void averageMarkOnSubjectInGroup(List<Student> group, String subject) throws MarkValueExceptions, NullAmountException {
+        double sum = 0.0;
+        int amountOfMarks = 0;
+
+        if (group.isEmpty()) {
+            throw new NullAmountException("Group " + groupName + " is empty");
+        }
+
+        for (Student student : group) {
+            boolean markValueIsNotValid = (student.getMarksAndSubjects().values().iterator().next() < 0.0 || student.getMarksAndSubjects().values().iterator().next() > 10.0);
+
+            if (student.getMarksAndSubjects().isEmpty()) {
+                throw new NullAmountException("Student " + student.getId() + " has no subjects");
+            }
+            if (markValueIsNotValid) {
+                throw new MarkValueExceptions("Invalid mark value.\nCheck student's " + student.getId() + " marks");
+            }
+            if (student.getMarksAndSubjects().containsKey(subject)) {
+                sum += student.getMarksAndSubjects().get(subject);
+                amountOfMarks++;
+            }
+        }
+
+        if (amountOfMarks == 0) {
+            System.out.println("\nThere is no subject " + subject + " in group " + groupName);
+        } else {
+            double averageMarkOnSubject = sum / amountOfMarks;
+            System.out.println("\nAverage mark on " + subject + " in group " + groupName + ": " + averageMarkOnSubject);
+        }
+    }
+
+
+   /*public void findStudentInGroup(List<Student> group) { //метод не работает!!!
         int counter = 0;
         System.out.println("Enter student id");
         Scanner userScanner = new Scanner(System.in);
@@ -233,59 +271,60 @@ public class University {
                 System.out.println("\nStudent id:" + student.getId());
                 try {
                     student.averageMark(student.getMarksAndSubjects());
-                } catch (MarkValueExceptions ex) {
+                } catch (MarkValueExceptions | NullAmountException ex) {
                     System.err.println(ex.getMessage());
                 }
                 counter++;
-            } else {
-                System.out.println("Student not found");
             }
+            }
+        if (counter==0){
+            System.out.println("Student not found");
         }
+    }*/
+}
+
+class Student extends University {
+
+    String id;
+    private Map<String, Double> marksAndSubjects;
+
+    public Student(String id, Map<String, Double> marksAndSubjects) {
+        this.id = id;
+        this.marksAndSubjects = marksAndSubjects;
     }
-}*/
 
-    class Student extends University {
+    public String getId() {
+        return id;
+    }
 
-        String id;
-        private Map<String, Double> marksAndSubjects;
+    public Map<String, Double> getMarksAndSubjects() {
+        return marksAndSubjects;
+    }
 
-        public Student(String id, Map<String, Double> marksAndSubjects) {
-            this.id = id;
-            this.marksAndSubjects = marksAndSubjects;
-        }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", marksAndSubjects=" + marksAndSubjects +
+                '}';
+    }
 
-        public String getId() {
-            return id;
-        }
-
-        public Map<String, Double> getMarksAndSubjects() {
-            return marksAndSubjects;
-        }
-
-        @Override
-        public String toString() {
-            return "Student{" +
-                    "id=" + id +
-                    ", marksAndSubjects=" + marksAndSubjects +
-                    '}';
-        }
-
-        public double averageMark(Map<String, Double> map) throws MarkValueExceptions, NullAmountException {
-            double sum = 0.0;
-            if (map.isEmpty()) {
-                throw new NullAmountException("Student has no subjects");
-            } else {
-                for (String mark : map.keySet()) {
-                    if (map.get(mark) < 0.0 || map.get(mark) > 10.0) {
-                        throw new MarkValueExceptions("Invalid mark value");
-                    } else {
-                        sum += map.get(mark);
-                    }
+    public double averageMark(Map<String, Double> map) throws MarkValueExceptions, NullAmountException {
+        double sum = 0.0;
+        if (map.isEmpty()) {
+            throw new NullAmountException("Student has no subjects");
+        } else {
+            for (String mark : map.keySet()) {
+                if (map.get(mark) < 0.0 || map.get(mark) > 10.0) {
+                    throw new MarkValueExceptions("Invalid mark value");
+                } else {
+                    sum += map.get(mark);
                 }
             }
-            return (sum / map.size());
         }
+        return (sum / map.size());
     }
+}
 
 
 
